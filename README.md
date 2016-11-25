1c9a2556ee848e23f2f672be05b00b07b3357ecd
Anton Gushcha <ncrashed@gmail.com>
2016-11-25 22:01:51 +0300

    Initial project setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # bread-units
+
+## Compilation
+
+1. Build frontend: `cd bread-units-frontend && ./build.sh`
+2. Build backend: `cd bread-units-backend && stack install`
+
+## Known problems
+
+* You need `cabal-install-1.24` in your path, not `1.25`.
---
+++ bread-units-frontend/build.sh
@@ -0,0 +1,10 @@
+# Replace current folder in stack.yaml
+cp stack.yaml.tmpl stack.yaml
+sed -i 's?CURRENT_FOLDER?'`pwd`'?' stack.yaml
+
+stack setup
+stack install "$@"
+
+# copy results
+rm -rf ../bread-units-backend/static/all.js
+cp $(stack path --local-install-root)/bin/client.jsexe/all.js ../bread-units-backend/static/all.js
